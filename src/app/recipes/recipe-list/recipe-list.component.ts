aa1fc0cc4c0b004342b1ee21f89ea949e5a54c4b
hritik Singh <hritiksingh812@gmail.com>
2021-06-22 15:04:10 +0530

    Initial Recipes component version

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe','This is a sample recipe','https://www.sciencenewsforstudents.org/wp-content/uploads/2020/11/1030_teen_brain_food_choices.jpg'),
+    new Recipe('Test Recipe','This is a sample recipe','https://www.sciencenewsforstudents.org/wp-content/uploads/2020/11/1030_teen_brain_food_choices.jpg'),
+    new Recipe('Test Recipe','This is a sample recipe','https://www.sciencenewsforstudents.org/wp-content/uploads/2020/11/1030_teen_brain_food_choices.jpg')
+  ];    //Array of Object of type class Recipe.
 
   constructor() { }
 
---
+++ src/app/recipes/recipe.model.ts
@@ -0,0 +1,12 @@
+//model class for the recipes
+export class Recipe {
+    public name: string;
+    public description:string;
+    public imagePath: string;
+
+    constructor(name:string, desc:string , imgPath:string){
+        this.name = name;
+        this.description = desc;
+        this.imagePath = imgPath;
+    }
+}
